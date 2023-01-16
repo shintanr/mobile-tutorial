5f05beb8f95d7b9d978964c2f9cbffac1bf1291c
shintanr <shintanurrohmah143@gmail.com>
2023-01-16 11:16:47 +0700

    margin and oadding

--- Flutter/flutter_app/lib/main.dart
+++ Flutter/flutter_app/lib/main.dart
@@ -25,10 +25,14 @@ class MyApp extends StatelessWidget {
               ]),
           body: SafeArea(
               child: Container(
+            margin:
+                EdgeInsets.only(left: 35.0, top: 20.0, right: 30.0, bottom: 10),
+            padding:
+                EdgeInsets.only(left: 10.0, top: 10.0, right: 10.0, bottom: 0),
             child: Column(
               children: <Widget>[
                 Image(
-                  image: AssetImage('assets/img/tani.jpg'),
+                  image: AssetImage('assets/img/paktani.png'),
                   height: 261,
                 ),
                 Text('Mari Bertani Bersama Kami'),
