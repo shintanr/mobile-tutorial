eb77d788a4c38dcbe8c10463743e211171167f8f
shintanr <shintanurrohmah143@gmail.com>
2023-01-20 23:30:32 +0700

    review ternary

--- Flutter/Dart/ternary.dart
+++ Flutter/Dart/ternary.dart
@@ -1,12 +1,14 @@
-void main(){
-    // condition ? expresi1 : exppresi2 (true/false)
-    // expres1 ?? expr2 (non-null)
+void main() {
+  // condition ? expresi1 : exppresi2 (true/false)
+  // expres1 ?? expr2 (non-null)
 
+  var angka = 5;
+  print((angka % 2 == 0) ? 'Genap' : 'Ganjil');
 
-    var angka = 5;
-    print((angka % 2 == 0) ? 'Genap' : 'Ganjil');
+  var number = null;
+  var number2 = number ?? 10;
+  print(number2);
 
-    var number = null;
-    var number2 = number ?? 10;
-    print(number2);
+  var angka1 = 4;
+  print((angka % 2 == 1) ? 'ganjil' : 'genap');
 }
