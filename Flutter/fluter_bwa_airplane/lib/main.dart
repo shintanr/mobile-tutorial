e034e292ff956fbdb292965f6b7d36383eb411d3
shintanr <shintanurrohmah143@gmail.com>
2023-01-28 09:30:23 +0700

    navigate and routes 2

--- Flutter/fluter_bwa_airplane/lib/main.dart
+++ Flutter/fluter_bwa_airplane/lib/main.dart
@@ -9,9 +9,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashPage(),
+      routes: {
+        '/': (context) => const SplashPage(),
+        '/get-started': (context) => const GetStartedPage(),
+      },
     );
   }
 }
