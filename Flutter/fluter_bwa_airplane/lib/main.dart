53e3822ba38536c6dfefa066bb75665ae63612db
shintanr <shintanurrohmah143@gmail.com>
2023-01-28 09:05:36 +0700

    slicing get started page

--- Flutter/fluter_bwa_airplane/lib/main.dart
+++ Flutter/fluter_bwa_airplane/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'ui/pages/splash_pages.dart';
+import 'ui/pages/get_started_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashPage(),
+      home: GetStartedPage(),
     );
   }
 }
