f0068ab526dccd686a0f1babd1ef652e0f28124a
shintanr <shintanurrohmah143@gmail.com>
2023-02-14 07:49:26 +0700

    add assets and theme

---
+++ Flutter/flutter_learn_slicing/lib/shared/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultRadius = 30.0;
+
+Color kPrimary = Color(0xffFFFFFF);
+Color kSecondary = Color.fromRGBO(235, 235, 245, 0.6);
+Color kTertiary = Color.fromRGBO(235, 235, 245, 0.3);
+Color kquarterary = Color.fromRGBO(235, 235, 245, 0.18);
+Color k = Color(0xff48319D);
